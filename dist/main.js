36024395a286d9068a2d1fac0c94bed037920534
Adam <adamskoullos@gmail.com>
2022-03-29 19:27:01 +0100

    Set TS dojo up with compiler and index.html

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,1 @@
-const user = {
-    name: "Dave",
-    age: 20,
-    getMessage() {
-        return `Hello ${this.name}`;
-    },
-};
-console.log(user.getMessage());
+console.log("Firing >>>>>>>>>>>>>>>>>>>>>>>>>");
